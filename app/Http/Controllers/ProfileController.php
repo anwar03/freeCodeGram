5b73d156f0b51a0e66406b8822326cbc165c4300
anwar03 <anwarazim03@gmail.com>
2020-09-12 12:45:17 +0600

    Editing the Profile Image

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Profile;
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
@@ -15,12 +16,14 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function edit(User $user){
+    public function edit(User $user)
+    {
         $this->authorize('update', $user->profile);
         return view('profile.edit', compact('user'));
     }
 
-    public function update(User $user){
+    public function update(User $user)
+    {
         $this->authorize('update', $user->profile);
 
         $data = request()->validate([
@@ -30,7 +33,16 @@ class ProfileController extends Controller
             'image' => ''
         ]);
 
-        auth()->user()->profile->update($data);
-        return redirect('/profile/'. $user->id);
+        $imagePath = request('image')->store('profile', 'public');
+
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+
+        $image->save();
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
+        return redirect('/profile/' . $user->id);
     }
 }
